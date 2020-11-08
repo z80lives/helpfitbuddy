b701837cfde3dbd18ccaa057f9e46d3bbc7b7ead
B1700444 <35720360+B1700444@users.noreply.github.com>
2020-11-08 23:34:41 +0800

    Outside stuff

--- fitbuddy/App.js
+++ fitbuddy/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import {Text, View} from "react-native";
-import LoginSreen from "./screens/login/Login"
+import { Alert, StyleSheet, SafeAreaView, Button, TextInput, View } from 'react-native';
 
-const App = () =>
-   (
-      
-   <LoginSreen></LoginSreen>
-    
-  );;
+export default function App () {
+        return (
+           
+          <SafeAreaView style={styles.container}>
+          
+            <View>
+            
+            <TextInput
+            placeholder="Type here to translate!"
+           />
+           </View>
+            <Button 
+           title="Click Me"
+           onPress={() => 
+           Alert.alert("My title", "My Message", [
+             {text: "Yes"},
+             {text: "No"},
+           ])
 
-export default App;
+    }
+        />
+          </SafeAreaView>
+          
+        );
+        <View>Hothaifa</View>
+      }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
+            
